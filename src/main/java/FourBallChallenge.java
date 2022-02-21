f650e6dfbe56f26b5b4bd51ef7a242c1db7bc37d
SSK-14 <sanjaykumar1481999@gmail.com>
2022-02-21 11:15:31 +0530

    Procedural Solution for four Ball challenge

---
+++ src/main/java/FourBallChallenge.java
@@ -0,0 +1,27 @@
+import processing.core.PApplet;
+
+public class FourBallChallenge extends PApplet {
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+    public static final int DIAMETER = 12;
+    int xCoordinate = 0;
+
+    public static void main(String[] args) {
+        PApplet.main("FourBallChallenge", args);
+    }
+
+    @Override
+    public void settings() {
+        super.settings();
+        size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void draw() {
+        ellipse(xCoordinate,HEIGHT/5, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 2,HEIGHT/5 * 2, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 3,HEIGHT/5 * 3, DIAMETER, DIAMETER);
+        ellipse(xCoordinate * 4,HEIGHT/5 * 4, DIAMETER, DIAMETER);
+        xCoordinate=xCoordinate+1;
+    }
+}
